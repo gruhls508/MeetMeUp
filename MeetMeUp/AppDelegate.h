489b0fb95c7ba43c4888d917f0a4f6ab1b70c991
Glen Ruhl <glenruhl@Glens-MacBook-Air.local>
2014-08-04 13:03:50 -0500

    Initial Commit

---
+++ MeetMeUp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MeetMeUp
+//
+//  Created by Glen Ruhl on 8/4/14.
+//  Copyright (c) 2014 MM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MeetMeUp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MeetMeUp
+//
+//  Created by Glen Ruhl on 8/4/14.
+//  Copyright (c) 2014 MM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
