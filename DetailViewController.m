f7b04e843455098bd9218a10605bc9c9a3de7452
Glen Ruhl <glenruhl@Glens-MacBook-Air.local>
2014-08-04 21:08:34 -0500

    rsvp label works, did a little cleanup

--- DetailViewController.m
+++ DetailViewController.m
@@ -10,7 +10,7 @@
 
 @interface DetailViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *eventNameLabel;
-//@property (weak, nonatomic) IBOutlet UILabel *rsvpCountsLabel;
+@property (weak, nonatomic) IBOutlet UILabel *rsvpLabel;
 @property (weak, nonatomic) IBOutlet UILabel *groupInfoLabel;
 @property (weak, nonatomic) IBOutlet UITextView *eventDescriptionView;
 @property (weak, nonatomic) IBOutlet UIButton *websiteLinkButton;
@@ -21,9 +21,6 @@
 
 
 
-
-
-
 - (void)viewDidLoad
 
 {
@@ -31,19 +28,24 @@
 
     NSURL *url = [NSURL URLWithString:
                   @"https://api.meetup.com/2/open_events.json?zip=60604&text=mobile&time=,1w&key=5f537f3357d2729651f11773e1e57"];
+    
+    
     NSURLRequest *request = [NSURLRequest requestWithURL:url];
-    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:
-     ^(NSURLResponse *rsp, NSData *data, NSError *connectionError)
-     {
+    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue]
+    completionHandler:^(NSURLResponse *rsp, NSData *data, NSError *connectionError){
          
-         self.eventNameLabel.text = [self.detailEventDictionary objectForKey:@"name"];
-         self.groupInfoLabel.text = [[self.detailEventDictionary valueForKey:@"group"]
+        
+        self.eventNameLabel.text = [self.detailEventDictionary objectForKey:@"name"];
+        self.groupInfoLabel.text = [[self.detailEventDictionary valueForKey:@"group"]
                                                                     valueForKey:@"name"];
-         self.eventDescriptionView.text = [self.detailEventDictionary objectForKey:@"description"];
-//    NSString *rsvpNumber = [NSString stringWithFormat:@"%i", [self.detailEventDictionary valueForKey:@"yes_rsvp_count"]];
-//    
-//    self.rsvpCountsLabel.text = rsvpNumber;
-//    
+        self.eventDescriptionView.text = [self.detailEventDictionary objectForKey:@"description"];
+         
+        NSLog(@"%@", [self.detailEventDictionary objectForKey:@"yes_rsvp_count"]);
+        
+        NSString *rsvpNumber = [NSString stringWithFormat:@"%@", [self.detailEventDictionary valueForKey:@"yes_rsvp_count"]];
+    
+        self.rsvpLabel.text = rsvpNumber;
+   
     
      }
      ];
