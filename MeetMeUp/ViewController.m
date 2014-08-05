c10825c8828e2090119c9cbd8c3026ae6091e65c
Glen Ruhl <glenruhl@Glens-MacBook-Air.local>
2014-08-04 21:58:55 -0500

    màs cleanup

--- MeetMeUp/ViewController.m
+++ MeetMeUp/ViewController.m
@@ -19,22 +19,24 @@
     NSIndexPath *currentPath;
 }
 
+
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    NSURL *url = [NSURL URLWithString:@"https://api.meetup.com/2/open_events.json?zip=60604&text=mobile&time=,1w&key=5f537f3357d2729651f11773e1e57"];
+    NSURL *url = [NSURL URLWithString:
+                @"https://api.meetup.com/2/open_events.json?zip=60604&text=mobile&time=,1w&key=5f537f3357d2729651f11773e1e57"];
+    
     NSURLRequest *request = [NSURLRequest requestWithURL:url];
-    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:
-     ^(NSURLResponse *rsp, NSData *data, NSError *connectionError)
-     {
-         self.meetUpArray = [[NSJSONSerialization JSONObjectWithData:data options:0 error:nil]objectForKey:@"results"];
-         [self.tableView reloadData];
-         NSString *JSONString = [[NSString alloc] initWithData:data encoding:
-                                 NSUTF8StringEncoding];
+    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue]
+                           completionHandler:^(NSURLResponse *rsp, NSData *data, NSError *connectionError){
          
-         NSLog(@"%@", JSONString);
+         self.meetUpArray = [[NSJSONSerialization JSONObjectWithData:data options:0 error:nil]
+                                                                        objectForKey:@"results"];
      }];
 }
+
+
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
@@ -43,17 +45,20 @@
     cell.textLabel.text  = [meetUpInfo objectForKey:@"name"];
     cell.detailTextLabel.text = [[meetUpInfo valueForKey:@"venue"]valueForKey:@"address_1"];
     
-    
     return cell;
 }
+
+
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
     return self.meetUpArray.count;
 }
 
--(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
+
+
+
+-(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender{
     DetailViewController *vc = segue.destinationViewController;
     
     
@@ -63,13 +68,13 @@
      ^(NSURLResponse *rsp, NSData *data, NSError *connectionError)
     {
     
-        vc.detailEventArray = [[NSJSONSerialization JSONObjectWithData:data options:0 error:nil]objectForKey:@"results"];
+        vc.detailEventArray = [[NSJSONSerialization JSONObjectWithData:data options:0 error:nil]
+                                                                        objectForKey:@"results"];
         vc.detailEventArray = self.meetUpArray;
         
         
         vc.detailEventDictionary = [self.meetUpArray objectAtIndex:[self.tableView indexPathForSelectedRow].row];
-    }
-     ];
+    }];
 }
 
 
