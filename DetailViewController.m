8f2580e2a1f301ec2af08b36670f9e5f73b91236
Glen Ruhl <glen.ruhl.90@gmail.com>
2014-10-12 16:42:10 -0400

    Intradine properly displays in all copyright notices

--- DetailViewController.m
+++ DetailViewController.m
@@ -3,7 +3,7 @@
 //  MeetMeUp
 //
 //  Created by Glen Ruhl on 8/4/14.
-//  Copyright (c) 2014 MM. All rights reserved.
+//  Copyright (c) 2014 Intradine. All rights reserved.
 //
 
 #import "DetailViewController.h"
